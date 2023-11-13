0314e5d8eab3413c438fa92cfd088fbabbcf7ff2
Minh Tuan <looongtommy27@gmail.com>
2023-11-13 16:25:10 +0700

    'upload-image-to-predic'

--- server-iot/opencv-stream-video-to-web-main/webstreaming.py
+++ server-iot/opencv-stream-video-to-web-main/webstreaming.py
@@ -10,10 +10,18 @@ import datetime
 import imutils
 import time
 import cv2
+
 import paho.mqtt.client as mqtt
 
+import numpy as np
+import os
+
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+
 # MQTT broker information
-mqtt_broker = "192.168.1.10"
+mqtt_broker = "192.168.1.4"
 mqtt_port = 1883
 mqtt_topic = "iot"
 relay_topic = "relay/control"
@@ -166,6 +174,69 @@ def turn_off_auto():
 
     return jsonify(response_data)
 # ==============================================================================================
+filepath = 'D:\IOT\Plant-Leaf-Disease-Prediction\model_custom.h5'
+model = load_model(filepath)
+print(model)
+
+print("Model Loaded Successfully")
+
+def pred_tomato_dieas(tomato_plant):
+  test_image = load_img(tomato_plant, target_size = (128, 128)) # load image 
+  print("@@ Got Image for prediction")
+  
+  test_image = img_to_array(test_image)/255 # convert image to np array and normalize
+  test_image = np.expand_dims(test_image, axis = 0) # change dimention 3D to 4D
+  
+  result = model.predict(test_image) # predict diseased palnt or not
+  print('@@ Raw result = ', result)
+  
+  pred = np.argmax(result, axis=1)
+  print(pred)
+  if pred==0:
+      return "Tomato - Bệnh đốm vi khuẩn (Bacteria Spot Disease)", 'Tomato-Bacteria Spot.html'
+       
+  elif pred==1:
+      return "Tomato - Bệnh bạc lá sớm (Early Blight Disease)", 'Tomato-Early_Blight.html'
+        
+  elif pred==2:
+      return "Tomato - Khoẻ mạnh", 'Tomato-Healthy.html'
+        
+  elif pred==3:
+      return "Tomato - Bệnh mốc sương (Late Blight Disease)", 'Tomato - Late_blight.html'
+       
+  elif pred==4:
+      return "Tomato - Bệnh mốc lá (Leaf Mold Disease)", 'Tomato - Leaf_Mold.html'
+        
+  elif pred==5:
+      return "Tomato - Bệnh đốm lá Septoria (Septoria Leaf Spot Disease)", 'Tomato - Septoria_leaf_spot.html'
+        
+  elif pred==6:
+      return "Tomato - Bệnh đốm mục chấm (Target Spot Disease)", 'Tomato - Target_Spot.html'
+        
+  elif pred==7:
+      return "Tomato - Bệnh virus vàng lá xoăn (Yellow Leaf Curl Virus Disease)", 'Tomato - Tomato_Yellow_Leaf_Curl_Virus.html'
+  elif pred==8:
+      return "Tomato - Bệnh khảm virus (Tomato Mosaic Virus Disease)", 'Tomato - Tomato_mosaic_virus.html'
+        
+  elif pred==9:
+      return "Tomato - Bệnh nhện đỏ hai đốm (Two Spotted Spider Mite Disease)", 'Tomato - Two-spotted_spider_mite.html'
+
+
+@app.route("/predict", methods = ['GET','POST'])
+def predict():
+     if request.method == 'POST':
+        file = request.files['image'] # fet input
+        filename = file.filename        
+        print("@@ Input posted = ", filename)
+        
+        file_path = os.path.join('./upload/', filename)
+        file.save(file_path)
+
+        print("@@ Predicting class......")
+        pred, output_page = pred_tomato_dieas(tomato_plant=file_path)
+              
+        return render_template(output_page, pred_output = pred, user_image = file_path)
+# ==========================================================================================================
 @app.route("/view-camera")
 def view_camera():
 	# return the rendered template
